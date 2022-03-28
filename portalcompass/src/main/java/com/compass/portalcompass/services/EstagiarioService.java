5d0c31f4006d09e6d6849d77d80f85839b72cde3
Pedro Tavares <pedrotavaresgf@gmail.com>
2022-03-28 16:38:19 -0300

    adicionei o endpoint get (find all) no controller da entidade 'Estagiario'.

--- portalcompass/src/main/java/com/compass/portalcompass/services/EstagiarioService.java
+++ portalcompass/src/main/java/com/compass/portalcompass/services/EstagiarioService.java
@@ -1,10 +1,15 @@
 package com.compass.portalcompass.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.compass.portalcompass.dto.EstagiarioDTO;
 import com.compass.portalcompass.dto.EstagiarioFormDTO;
 
 public interface EstagiarioService {
 
 	EstagiarioDTO insert(EstagiarioFormDTO estagiarioBody);
+
+	Page<EstagiarioDTO> findAll(Pageable pageable);
 
 }
--- portalcompass/src/main/java/com/compass/portalcompass/services/EstagiarioServiceImp.java
+++ portalcompass/src/main/java/com/compass/portalcompass/services/EstagiarioServiceImp.java
@@ -2,8 +2,15 @@ package com.compass.portalcompass.services;
 
 
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.compass.portalcompass.dto.EstagiarioDTO;
@@ -25,6 +32,11 @@ public class EstagiarioServiceImp implements EstagiarioService {
 		Estagiario estagiario = repositorio.save(mapper.map(estagiarioBody, Estagiario.class));
 		return mapper.map(estagiario, EstagiarioDTO.class);
 	}
-	
-	
+
+	@Override
+	public Page<EstagiarioDTO> findAll(Pageable pageable) {
+		List<Estagiario> lista = repositorio.findAll();
+		List<EstagiarioDTO> listaDto = lista.stream().map(e -> mapper.map(e, EstagiarioDTO.class)).collect(Collectors.toList());
+		return new PageImpl<>(listaDto, pageable, listaDto.size());
+	}
 }
