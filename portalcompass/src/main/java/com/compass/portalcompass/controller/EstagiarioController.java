9976ecf19df39fb95147737d667cef68a79e26d2
vinicius muliterno <viniciusmuliternoti@hotmail.com>
2022-03-29 15:01:43 -0300

    Inicio da Entidade Sprint
    criando as classes

--- portalcompass/src/main/java/com/compass/portalcompass/controller/EstagiarioController.java
+++ portalcompass/src/main/java/com/compass/portalcompass/controller/EstagiarioController.java
@@ -4,13 +4,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
---
+++ portalcompass/src/main/java/com/compass/portalcompass/repositories/SprintRepositorio.java
@@ -0,0 +1,11 @@
+package com.compass.portalcompass.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.compass.portalcompass.entities.Sprint;
+
+@Repository
+public interface SprintRepositorio extends JpaRepository< Sprint ,Long>{
+
+}
