6655c7b09fe8e4d1ef1a84409b77b2aa3ff9ff55
Matheus Braynner <m.braynner@outlook.com>
2022-03-31 19:55:52 -0300

    seila o que

--- portalcompass/src/main/java/com/compass/portalcompass/services/MaterialDeEstudoService.java
+++ portalcompass/src/main/java/com/compass/portalcompass/services/MaterialDeEstudoService.java
@@ -2,6 +2,8 @@ package com.compass.portalcompass.services;
 
 
 
+import org.springframework.data.domain.Page;
+
 import com.compass.portalcompass.dto.MaterialDeEstudoDTO;
 import com.compass.portalcompass.dto.MaterialDeEstudoForm;
 
@@ -9,5 +11,7 @@ import com.compass.portalcompass.dto.MaterialDeEstudoForm;
 public interface MaterialDeEstudoService {
 
 	MaterialDeEstudoDTO insert(MaterialDeEstudoForm materialDeEstudoBody);
+
+	Page<MaterialDeEstudoDTO> findAll(int size, int page, String sort);
 
 }
