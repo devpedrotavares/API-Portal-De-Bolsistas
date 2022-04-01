5d2854940b2985fa2fc8388708e67c83f6bb9475
Matheus Braynner <m.braynner@outlook.com>
2022-03-31 22:23:02 -0300

    Endpoint get temas/{id}/sprint, a partir do id de tema, ele mostra a sprint do determinado tema

--- portalcompass/src/main/java/com/compass/portalcompass/controllers/TemaController.java
+++ portalcompass/src/main/java/com/compass/portalcompass/controllers/TemaController.java
@@ -2,8 +2,6 @@ package com.compass.portalcompass.controllers;
 
 import javax.transaction.Transactional;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -18,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.compass.portalcompass.dto.SprintDTO;
 import com.compass.portalcompass.dto.TemaDTO;
 import com.compass.portalcompass.dto.TemaFormDTO;
 import com.compass.portalcompass.dto.VinculoTemaSprintForm;
@@ -29,46 +28,51 @@ public class TemaController {
 
 	@Autowired
 	private TemaService service;
-	
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity<TemaDTO> insert(@RequestBody TemaFormDTO temaBody) {
 		TemaDTO tema = service.insert(temaBody);
 		return ResponseEntity.status(HttpStatus.CREATED).body(tema);
 	}
-	
+
 	@GetMapping
-	public Page<TemaDTO> findAll(@RequestParam(defaultValue = "10") int size, 
-			@RequestParam(defaultValue = "0") int page, 
-			@RequestParam(required = false) String sort) {
+	public Page<TemaDTO> findAll(@RequestParam(defaultValue = "10") int size,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(required = false) String sort) {
 		return service.findAll(size, page, sort);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TemaDTO> findById(@PathVariable Long id) {
 		TemaDTO tema = service.findById(id);
 		return ResponseEntity.ok(tema);
 	}
 	
+	@GetMapping(value = "/{id}/sprint")
+	public ResponseEntity<SprintDTO> findSprintByIdTema(@PathVariable Long id) {
+		SprintDTO sprint = service.findSprintByIdTema(id);
+		return ResponseEntity.ok().body(sprint);	
+	}
+
 	@PutMapping(value = "/{id}")
 	@Transactional
 	public ResponseEntity<TemaDTO> update(@PathVariable Long id, @RequestBody TemaFormDTO temaBody) {
 		TemaDTO tema = service.update(id, temaBody);
 		return ResponseEntity.ok(tema);
 	}
-	
-	//vincula o material de estudo ao tema
-		@PutMapping(value = "/sprint")
-		@Transactional
-		public ResponseEntity<?> vincularASprint(@RequestBody VinculoTemaSprintForm form){
-			service.vincularSprint(form);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}
-	
+
+	// vincula o material de estudo ao tema
+	@PutMapping(value = "/sprint")
+	@Transactional
+	public ResponseEntity<?> vincularASprint(@RequestBody VinculoTemaSprintForm form) {
+		service.vincularSprint(form);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
 	@DeleteMapping(value = "/{id}")
 	@Transactional
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-}	
+	}
 }
