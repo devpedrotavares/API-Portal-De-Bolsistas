5ad4aea7fd1d4adff5eb3210d75277bfaa3b6153
Matheus Braynner <m.braynner@outlook.com>
2022-04-06 21:20:47 -0300

    removendo pastas inuteis

--- email/src/main/java/com/compass/email/enums/StatusEmail.java
+++
@@ -1,6 +0,0 @@
-package com.compass.email.enums;
-
-public enum StatusEmail {
-	SENT,
-	ERROR;
-}
--- email/src/main/java/com/compass/email/services/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.compass.email.services;
-
-import com.compass.email.entities.Email;
-
-public interface EmailService {
-
-	Email sendEmail(Email email);
-}
