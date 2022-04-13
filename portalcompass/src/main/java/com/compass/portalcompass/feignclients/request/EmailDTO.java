6fe2d2614f49b91be5ce9abbf970ce4f9279a1f6
Pedro Tavares <pedrotavaresgf@gmail.com>
2022-04-13 13:08:34 -0300

    envio automatico de email

--- portalcompass/src/main/java/com/compass/portalcompass/feignclients/request/EmailDTO.java
+++ portalcompass/src/main/java/com/compass/portalcompass/feignclients/request/EmailDTO.java
@@ -1,5 +1,8 @@
 package com.compass.portalcompass.feignclients.request;
 
+import com.compass.portalcompass.entities.Estagiario;
+import com.compass.portalcompass.entities.EstagiarioSprint;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +17,19 @@ public class EmailDTO {
 	private String emailTo;
 	private String subject;
 	private String text;
+	
+	public EmailDTO(EstagiarioSprint vinculo) {
+		Estagiario estagiario = vinculo.getEstagiario();
+		
+		this.ownerRef = estagiario.getNome();
+		this.emailFrom = "compasstestemailapi@gmail.com";
+		this.emailTo = estagiario.getEmail();
+		this.subject = "teste";
+		this.text = 
+				String.format("Bom dia, %s! Segue os resultados da avaliação. Nota comportamental: %s. Nota técnica: %s. Temas de reforço: %s", 
+						estagiario.getNome(), 
+						vinculo.getNotaComportamental().toString(), 
+						vinculo.getNotaTecnica().toString(),
+						vinculo.getTemasReforco().toString());
+	}
 }
