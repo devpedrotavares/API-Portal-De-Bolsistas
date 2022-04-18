ed8c4a61c1858387d94825adc33ac34c2fc2a1c0
Pedro Tavares <pedrotavaresgf@gmail.com>
2022-04-18 18:33:48 -0300

    adicionei mais um estagiário autenticado

--- portalcompass/src/main/resources/data.sql
+++ portalcompass/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 /*Usuários */
-INSERT INTO USUARIO(id, nome, email, senha) VALUES (1, 'USUARIO', 'instrutor@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
-INSERT INTO USUARIO(id, nome, email, senha) VALUES (2, 'USUARIO', 'estagiario@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USUARIO(id, nome, email, senha) VALUES (1, 'admin', 'instrutor@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USUARIO(id, nome, email, senha) VALUES (2, 'estagiario', 'estagiario@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO USUARIO(id, nome, email, senha) VALUES (3, 'estagiario2', 'portalcompasstesteapie@outlook.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
 
 /*Perfis */
 INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ADMIN');
@@ -9,3 +10,4 @@ INSERT INTO PERFIL(id, nome) VALUES (2, 'ROLE_ESTAGIARIO');
 /*Relacionamento*/
 INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1, 1);
 INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (2, 2);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (3, 2);
